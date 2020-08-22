2a67c8b2d43f7b77bf9cd4ffe31a852356244a38
allielynne20 <allielynne20@gmail.com>
2020-08-21 21:48:56 -0400

    finialized schema and seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,8 @@
-INSERT INTO  (name, description)
-VALUES ();
+INSERT INTO department (name)
+VALUES ('Sales'), ('Finance'), ('Engineers'), ('HR'), ('Marketing');
 
-INSERT INTO  (name, description)
-VALUES ();
+INSERT INTO role (title, salary, department_id)
+VALUES ('Admin', 45000, 1), ('Secretary', 35000, 2), ('Engineer', 65000, 3), ('HR Rep', 45000, 4), ('Marketing Agent', 35000, 5);
 
-INSERT INTO  (name, description)
-VALUES ();
-
-INSERT INTO  (name, description)
-VALUES ();
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Joe', 'Smith', 1, 1), ('Mark', 'Taylor', 2, 2), ('Sally', 'Webber', 3, 3), ('Casey', 'Rogers', 4, 4), ('Parker', 'Walters', 5, 5); 
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-//use this file to call thos class methods to diplay the tables using console.table in command-line 
+//use this file to call those class methods to display the tables using console.table in command-line 
 
 //put inquirer prompts in this file because this will be the file you run in command-line 'node index.js'
+const inquirer = require('inquirer');
